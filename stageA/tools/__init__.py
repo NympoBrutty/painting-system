0db0d730ddcf6019abe6672ee57eed0a453f6f3b
Німпо Брутті <merdissimo@gmail.com>
2025-12-25 18:20:53 +0200

    Initialize stageA tools package with CLI utilities

---
+++ stageA/tools/__init__.py
@@ -0,0 +1,12 @@
+"""
+Stage A Tools Package
+
+CLI utilities for contract management:
+    - batch_validator: Validate multiple contracts
+    - generate_from_template: Create new contracts from template
+"""
+
+__all__ = [
+    "batch_validator",
+    "generate_from_template",
+]
